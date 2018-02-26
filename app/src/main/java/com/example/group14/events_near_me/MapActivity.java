456ddcc044498f13ac68599c790cf9cf68488751
Ben Rogers <btorogers@live.co.uk>
2018-02-26 22:50:21 +0000

    aaah

--- app/src/main/java/com/example/group14/events_near_me/MapActivity.java
+++ app/src/main/java/com/example/group14/events_near_me/MapActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MapActivity extends AppCompatActivity {
+    FirebaseController database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+
     }
 }
--- app/src/main/java/com/example/group14/events_near_me/SignInActivity.java
+++ app/src/main/java/com/example/group14/events_near_me/SignInActivity.java
@@ -1,7 +1,6 @@
 package com.example.group14.events_near_me;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,35 +11,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
     private GoogleSignInClient googleClient;
-    private FirebaseController database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signin);
 
-        database = new FirebaseController();
-        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.server_client_id))
-                .requestProfile()
-                .build();
-        googleClient = GoogleSignIn.getClient(this, options);
-
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (account != null) {
-            ((TextView)findViewById(R.id.testText)).setText(account.getDisplayName());
-            findViewById(R.id.sign_in_button).setEnabled(false);
-            database.authenticate(account);
-        }
+        googleClient = ((EventsApplication)this.getApplication()).getClient();
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
         findViewById(R.id.sign_out_button).setOnClickListener(this);
@@ -69,10 +49,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
+                ((EventsApplication)this.getApplication()).getDatabase().authenticate(account);
 
-                database.authenticate(account);
-                ((TextView)findViewById(R.id.testText)).setText(account.getDisplayName());
-                findViewById(R.id.sign_in_button).setEnabled(false);
+                Intent intent = new Intent(this, MapActivity.class);
+                finish();
+                startActivity(intent);
             } catch (ApiException e) {
                 e.printStackTrace();
                 ((TextView)findViewById(R.id.testText)).setText("Error");
