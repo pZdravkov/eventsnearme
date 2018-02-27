b72963b4910c33b3783fa2917e8dadbb683d8a47
Ben Rogers <btorogers@live.co.uk>
2018-02-27 20:58:22 +0000

    sign in asks user for information. Info is then saved in firebase

--- app/src/main/java/com/group14/events_near_me/FirebaseController.java
+++ app/src/main/java/com/group14/events_near_me/FirebaseController.java
@@ -1,5 +1,7 @@
 package com.group14.events_near_me;
 
+import android.content.SharedPreferences;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,6 +17,7 @@ public class FirebaseController {
     FirebaseAuth firebaseAuth;
     FirebaseDatabase database;
     DatabaseReference ref;
+    String currentUserId;
 
     public FirebaseController() {
         firebaseAuth = FirebaseAuth.getInstance();
@@ -25,5 +28,16 @@ public class FirebaseController {
     public void authenticate(GoogleSignInAccount acct) {
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         firebaseAuth.signInWithCredential(credential);
+    }
+
+    public String addUser(User user) {
+        currentUserId = ref.child("users").push().getKey();
+        ref.child("users").child(currentUserId).setValue(user);
+
+        return currentUserId;
+    }
+
+    public void setCurrentUserId(String currentUserId) {
+        this.currentUserId = currentUserId;
     }
 }
--- app/src/main/java/com/group14/events_near_me/SignInActivity.java
+++ app/src/main/java/com/group14/events_near_me/SignInActivity.java
@@ -1,9 +1,14 @@
 package com.group14.events_near_me;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.EventLog;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -18,15 +23,22 @@ import com.google.android.gms.tasks.Task;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
     private GoogleSignInClient googleClient;
+    private GoogleSignInAccount account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
-        googleClient = ((EventsApplication)this.getApplication()).getClient();
+        googleClient = ((EventsApplication)getApplication()).getClient();
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
-        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.confirmDetails).setOnClickListener(this);
+
+        boolean isSignedIn = getIntent().getBooleanExtra("isAuthenticated", false);
+        if (isSignedIn) {
+            account = ((EventsApplication)getApplication()).getAccount();
+            setupDetailsEntry();
+        }
     }
 
     @Override
@@ -36,11 +48,19 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
                 Intent signIn = googleClient.getSignInIntent();
                 startActivityForResult(signIn, 123);
                 break;
-            case R.id.sign_out_button:
-                googleClient.signOut();
+            case R.id.confirmDetails:
+                boolean gender = ((CheckBox)findViewById(R.id.genderSelect)).isChecked();
+                int age = ((NumberPicker)findViewById(R.id.ageSelect)).getValue();
+                User user = new User(account.getGivenName(), account.getFamilyName(), gender, age, account.getIdToken());
+                String userID = ((EventsApplication)this.getApplication()).getDatabase().addUser(user);
+                // write userID to shared preferences
+                SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE).edit();
+                editor.putString(getString(R.string.preference_id_key), userID);
+                editor.apply();
+
+                Intent intent = new Intent(this, MapActivity.class);
                 finish();
-                startActivity(getIntent());
-                break;
+                startActivity(intent);
         }
     }
 
@@ -51,16 +71,26 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         if (requestCode == 123) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
+                account = task.getResult(ApiException.class);
                 ((EventsApplication)this.getApplication()).getDatabase().authenticate(account);
 
-                Intent intent = new Intent(this, MapActivity.class);
-                finish();
-                startActivity(intent);
+                setupDetailsEntry();
             } catch (ApiException e) {
                 e.printStackTrace();
                 ((TextView)findViewById(R.id.testText)).setText("Error");
             }
         }
+    }
+
+    private void setupDetailsEntry() {
+        findViewById(R.id.ageText).setVisibility(View.VISIBLE);
+        NumberPicker numberPicker = (NumberPicker)findViewById(R.id.ageSelect);
+        numberPicker.setVisibility(View.VISIBLE);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(100);
+        findViewById(R.id.genderText).setVisibility(View.VISIBLE);
+        findViewById(R.id.genderSelect).setVisibility(View.VISIBLE);
+        findViewById(R.id.confirmDetails).setVisibility(View.VISIBLE);
+        findViewById(R.id.sign_in_button).setEnabled(false);
     }
 }
